7de25c486489566ae0d2e94aeb8b9b0de4f498e0
rahad404 <rahad.0911@gmail.com>
2026-04-17 19:09:35 +0600

    add: timeline contex: main page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,14 +8,34 @@ import LoadingSpinner from '@/components/ui/LoadingSpinner'
 import friendsData from '@/data/friends.json'
 
 export default function HomePage() {
+  const [friends, setFriends] = useState([])
+  const [loading, setLoading] = useState(true)
 
+  useEffect(() => {
+    setTimeout(() => {
+      setFriends(friendsData)
+      setLoading(false)
+    }, 1000)
+  }, [])
+
+  if (loading) {
+    return <LoadingSpinner />
+  }
 
   return (
     <div className="container mx-auto py-8">
-
+      {/* <Banner /> */}
+      {/* <SummaryCards friends={friends} /> */}
+      
       <h2 className="text-2xl font-bold text-gray-800 mt-12 mb-6">
         Your Friends
-      </h2>      
+      </h2>
+      
+      {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        {friends.map((friend) => (
+          <FriendCard key={friend.id} friend={friend} />
+        ))}
+      </div> */}
     </div>
   )
 }
