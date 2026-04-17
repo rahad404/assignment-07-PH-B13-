480b4de60a728ba5e5e91781aea5ce775e9f46e4
rahad404 <rahad.0911@gmail.com>
2026-04-17 19:06:28 +0600

    add: Layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,8 @@
 import './globals.css'
 import Navbar from '@/components/layout/Navbar'
 import Footer from '@/components/layout/Footer'
-// import { TimelineProvider } from '@/context/TimelineContext'
-// import { Toaster } from 'react-hot-toast'
+import { TimelineProvider } from '@/context/TimelineContext'
+import { Toaster } from 'react-hot-toast'
 
 export const metadata = {
   title: 'KeenKeeper - Keep Your Friendships Alive',
@@ -13,11 +13,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="min-h-screen flex flex-col">
+        <TimelineProvider>
           <Navbar />
           <main className="flex-grow">
             {children}
           </main>
           <Footer />
+          <Toaster position="top-right" />
+        </TimelineProvider>
       </body>
     </html>
   )
